500867cdb00a313825c1f46e6dd62c9fb8ebbbbf
khuong <khuongnguyen232@gmail.com>
2020-03-17 17:19:02 -0700

    add check question func

--- src/components/TenQuestion/QuestionCard.js
+++ src/components/TenQuestion/QuestionCard.js
@@ -6,48 +6,75 @@ function shuffle(array) {
   return array.sort(() => Math.random() - 0.5);
 }
 
-const QuestionCard = ({question}) => {
-  if(!question) {
-    return <div>Loading data ... </div>
-  } else {
-    const answerList = question.incorrect_answers;
-    answerList.push(question.correct_answer);
-    const booleanList = ['True','False'];
+class QuestionCard extends React.Component {
+  state={isAnswered:false, answerList:[], booleanList:['True','False']};
 
-    const displayList = (array) => {
-      return array.map( (answer) => {
-        return <div className="card__answer">{answer}</div>
-      })
+  componentDidMount() {
+    let tempList = this.props.question.incorrect_answers;;
+    tempList.push(this.props.question.correct_answer);
+    this.setState({answerList:shuffle(tempList)});
+  }
+
+  checkAnswer= (event) => {
+    if(event.target.textContent === he.decode(this.props.question.correct_answer)) {
+      event.target.className = 'card__answer card__answer--true';
+    } else {
+      event.target.className = 'card__answer card__answer--false';
     }
+    this.setState({isAnswered:true});
+  }
 
-    if(question.type === "multiple") {
-      return(
-        <div className="card">
-          <div className="card__question">{he.decode(question.question)}</div>
-          <div className="card__answer-list">
-            {shuffle(displayList(answerList))}
-          </div>
+  render() {
+    const {question} = this.props;
+    if(!question) {
+      return <div>Loading data ... </div>
+    } else {
+      const displayList = (array) => {
+        return array.map( (answer) => {
+          return <button key={answer} className="card__answer" onClick={this.checkAnswer}>{he.decode(answer)}</button>
+        })
+      }
 
-          <div>
-            Cheat: {question.correct_answer}
-          </div>
-          <br />
-        </div>
-      );
-    }
+      if(question.type === "multiple") {
+        return(
+          <div className="card">
+            <div className="card__question">{he.decode(question.question)}</div>
+            <div className="card__answer-list">
+              {displayList(this.state.answerList)}
+            </div>
 
-    if(question.type === "boolean") {
-      return(
-        <div className="card">
-          <div className="card__question">{question.question}</div>
-          <div className="card__answer-list">
-            {displayList(booleanList)}
+            { //display answer after click buttons
+              this.state.isAnswered? (
+                <div>
+                  Cheat: {question.correct_answer}
+                </div>
+              ):<React.Fragment></React.Fragment>
+            }
+            <br />
           </div>
-          <br />
-        </div>
-      );
-    }
-  };
+        );
+      }
+
+      if(question.type === "boolean") {
+        return(
+          <div className="card">
+            <div className="card__question">{question.question}</div>
+            <div className="card__answer-list">
+              {displayList(this.state.booleanList)}
+            </div>
+            <br />
+            { //display answer after click buttons
+              this.state.isAnswered? (
+                <div>
+                  Cheat: {question.correct_answer}
+                </div>
+              ):<React.Fragment></React.Fragment>
+            }
+          </div>
+        );
+      }
+    };
+  }
 };
 
 export default QuestionCard;
