a727b63c543c383f0f42b463d097c8101466c8b0
khuong <khuongnguyen232@gmail.com>
2020-05-20 13:54:32 -0700

    set up error warning message

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -7,7 +7,7 @@ import Menu from './Menu';
 
 class Questions extends React.Component {
   //only display question when isSubmit is true
-  state= {questions:[], score:0,difficulty:null,isSubmit:false};
+  state= {questions:[], score:0,difficulty:null,isSubmit:false,isError:false};
 
   getQuestions = async () => {
     try {
@@ -17,13 +17,13 @@ class Questions extends React.Component {
           difficulty:this.state.difficulty
         }
       });
-
+      console.log(response);
       //check if response is successful
       if(response.status === 200) {
         this.setState({questions:response.data.results,isSubmit:true,score:0})
       }
     } catch (err) {
-      console.log(err);
+      this.setState({questions:[],score:0,isSubmit:true,isError:true})
     }
   }
 
@@ -39,15 +39,23 @@ class Questions extends React.Component {
   }
 
   render() {
-      return (
-        <div className="App">
-          <ScoreBoard score={this.state.score}/>
-          <Menu getQuestions={this.getQuestions} changeDifficulty={this.changeDifficulty}/>
-          {this.state.isSubmit?
-            <QuestionList list={this.state.questions} addScore={this.addScore}/>:
-            <div className="init-message">Please choose a difficulty and click "Load Questions" button to see the questions. Enjoy!</div>
-          }
-        </div>
+        console.log(this.state.isError);
+        let message;
+        if(this.state.isError) {
+          message = 'There is an issue with the server, please try again later';
+        } else {
+          message = 'Please choose a difficulty and click "Load Questions" button to display the questions. Enjoy!';
+        }
+
+        return(
+          <div className="App">
+            <ScoreBoard score={this.state.score}/>
+            <Menu getQuestions={this.getQuestions} changeDifficulty={this.changeDifficulty}/>
+            {this.state.isSubmit?
+              <QuestionList list={this.state.questions} addScore={this.addScore}/>:
+              <div className="init-message">{message}</div>
+            }
+          </div>
     )
   };
 };
